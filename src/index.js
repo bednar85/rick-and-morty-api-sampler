91e556edaf3563174bdd5d25d0c56ee3bfcb4366
Ryan Bednar <ryan_bednar@comcast.com>
2019-12-18 14:56:06 -0500

    add logic to load characters from API, render a div for each character

--- src/index.js
+++ src/index.js
@@ -1,9 +1,73 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let state = {};
+
+// merge the current state with the new state
+function setState(newState) {
+  state = {
+    ...state,
+    ...newState
+  };
+}
+
+function loadData(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { info, results } = data;
+
+      setState({ info, results });
+      renderResults(data.results);
+    })
+    .catch(error => {
+      console.log("Error: ", error);
+    });
+}
+
+
+
+function renderResults(results) {
+  document.getElementById("results").innerHTML = results
+    .map(
+      datum => `
+  <div>    
+    <img src="${datum.image}" width="160" height="160" />
+    <div>id: ${datum.id}</div>
+    <div>name: ${datum.name}</div>
+    <div>status: ${datum.status}</div>
+    <div>species: ${datum.species}</div>
+    <div>gender: ${datum.gender}</div>
+    <div>origin: ${datum.origin.name}</div>
+    <div>location: ${datum.location.name}</div>
+  </div>
+  `
+    )
+    .join("");
+}
+
+loadData("https://rickandmortyapi.com/api/character/");
+
+
+const paginationElement = document.querySelector('.pagination-controls');
+
+paginationElement.addEventListener(
+  "click",
+  (event) => {
+    if (
+      event.target.matches(".pagination-controls-btn--prev") &&
+      state.info.prev
+    ) {
+      loadData(state.info.prev);
+    }
+
+    if (
+      event.target.matches(".pagination-controls-btn--next") &&
+      state.info.next
+    ) {
+      loadData(state.info.next);
+    }
+
+    console.log("state:", state);
+  },
+  false
+);
