19d8abe6fec4ae873514d353063df7885a87d142
Ryan Bednar <ryan_bednar@comcast.com>
2019-12-19 11:37:04 -0500

    increase the data fetching

--- src/index.js
+++ src/index.js
@@ -57,7 +57,9 @@ function loadCharacterData() {
   Promise.all([
     fetch('https://rickandmortyapi.com/api/character/?page=1'),
     fetch('https://rickandmortyapi.com/api/character/?page=2'),
-    fetch('https://rickandmortyapi.com/api/character/?page=3')
+    fetch('https://rickandmortyapi.com/api/character/?page=3'),
+    fetch('https://rickandmortyapi.com/api/character/?page=4'),
+    fetch('https://rickandmortyapi.com/api/character/?page=5')
   ]).then(responses => Promise.all(responses.map(response => response.json()))
   ).then(data => {
     // combine all of the results into one array
