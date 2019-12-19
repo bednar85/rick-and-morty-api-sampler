978c53fac1154299c518bda661e11ef6efb4b4dd
Ryan Bednar <ryan_bednar@comcast.com>
2019-12-19 01:08:58 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ function setResults(results) {
 function loadCharacterData() {
   Promise.all([
     fetch('https://rickandmortyapi.com/api/character/?page=1'),
+    fetch('https://rickandmortyapi.com/api/character/?page=2'),
     fetch('https://rickandmortyapi.com/api/character/?page=3')
   ]).then(responses => Promise.all(responses.map(response => response.json()))
   ).then(data => {
