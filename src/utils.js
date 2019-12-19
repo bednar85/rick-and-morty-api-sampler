59c8b399374bab94972712b9c34a1aa41852f655
Ryan Bednar <ryan_bednar@comcast.com>
2019-12-19 14:33:18 -0500

    add sorting utils

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,31 @@ export const chunk = (arr, chunkSize = 1, cache = []) => {
   while (tmp.length) cache.push(tmp.splice(0, chunkSize))
   return cache
 }
+
+export const compare = (a, b) => {
+  if (a < b) {
+    return -1;
+  }
+
+  if (a > b) {
+    return 1;
+  }
+
+  return 0;
+}
+
+export const sort = (data = [], direction = 'ascending') => {
+  return data.slice().sort((a, b) => {
+    compare(a.toString().toLowerCase(), b.toString().toLowerCase());
+  });
+}
+
+export const sortByArrayLength = (data = [], direction = 'ascending', arrayKey) => {
+  const sortedData = data.slice().sort((a, b) => compare(a[arrayKey].length, b[arrayKey].length));
+
+  if (direction === 'descending') {
+    return sortedData.reverse();
+  }
+
+  return sortedData;
+}
